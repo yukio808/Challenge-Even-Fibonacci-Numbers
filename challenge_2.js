19bf6e202013dfde040d31ac3ef7dd6482f70414
yukio808 <aj.yamamoto@me.com>
2015-01-30 18:57:03 -1000

    added files for trans

--- challenge_2.js
+++ challenge_2.js
@@ -1,17 +1,21 @@
 exports.sumOfEvenFibonacciNumbers = function(n){
   var fib = [1,2,3,5,8,13,21,34,55,89];
+
+
 //.map 
-
-{  // [1,2,3,4,5,6,7,8,9,10]
-   fib.map( function ( current ){
-     return [i-2]+[i-1];
+// [1,2,3,4,5,6,7,8,9,10]
+   
+var addon_fib = fib.map( function ( current, i, array ){
+     if (i <= n){
+     return array[i-1]+ array[i];
+   };
    });
+console.log(addon_fib);
 
-  };
 
 //.filter
   var even_fib = fib.filter( function ( current ){
-    return current % 2 === 0;
+    return current % 2 === 0 && current < n;
   });
     console.log(even_fib);
 
@@ -20,5 +24,13 @@ exports.sumOfEvenFibonacciNumbers = function(n){
   var sum_fib = even_fib.reduce( function ( previous , current ){
     return previous + current;
   });
-    console.log('SUM TOTAL IS: ', sum_fib);
+    console.log("SUM TOTAL IS: ", sum_fib);
+
 };
+var fib = [1,2,3,5,8,13,21,34,55,89];
+var continue_fib = fib.map (function ( current, i, array ){
+     if (i <= 4613732){
+     return array[i-1]+ array[i];
+   };
+});
+return sum_fib;
--- spec/challenge_2-spec.js
+++ spec/challenge_2-spec.js
@@ -4,15 +4,15 @@ describe("Sum of even numbers in the fibonnaci sequence", function () {
   
   it("should return '10' for n=10", function () {
     var sum = challenge_2.sumOfEvenFibonacciNumbers(10);
-    expect(10).toBe(sum);
+    expect(sum).toBe(10);
   });
 
-  // when your tests pass, uncomment the next test and
+//  when your tests pass, uncomment the next test and
   // change the expected value to the correct result
   
-  // it("should return 'X' for n=4,000,000", function () {
-  //   var sum = challenge_2.sumOfEvenFibonacciNumbers(4000000);
-  //   expect(X).toBe(sum); // CHANGE THE VALUE OF X to the correct number
-  // });
+    it("should return 'X' for n=4,000,000", function () {
+      var sum = challenge_2.sumOfEvenFibonacciNumbers(4000000);
+      expect(sum).toBe(4613732); // CHANGE THE VALUE OF X to the correct number
+    });
 
 });    
